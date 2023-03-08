7c45d1b2ab8df5e19d2dba5a2289e6dd282909fa
MFUSERhelp <127178868+MFUSERhelp@users.noreply.github.com>
2023-03-08 16:07:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import json
+from datetime import date, timedelta
 
 app = Flask(__name__)
 
@@ -10,19 +11,40 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def get_mf():
-    # scheme_code = str(request.args.get('input'))
-    API_ENDPOINT = r"https://pulsedb.pulselabs.co.in/rest/api/v1/screener/search"
+    scheme_code = str(request.args.get('input'))
+    # print("#########",scheme_code)
+    API_ENDPOINT1 = r"https://pulsedb.pulselabs.co.in/rest/api/v1/screener/search"
+    API_ENDPOINT2 = r"https://pulsedb-qa.pulselabs.co.in/rest/api/v1/mf/nav-history"
+    auth = "zR3w_WoBHZubHyRVAF0l"
+    to_date = str(date.today())
+    from_date = str(date.today()-timedelta(days=365))
     data = {
-            "auth": "zR3w_WoBHZubHyRVAF0l",
+            "auth": auth,
             "plan_name": "broker",
-            "scheme_code": "144342",
+            "scheme_code": scheme_code,
             "holding_period": "1Y"
             }
-    r = requests.post(url = API_ENDPOINT, data = data)
+    r = requests.post(url = API_ENDPOINT1, data = data)
     pastebin_url = r.text
-    json_object = json.loads(pastebin_url)
+    json_object = json.loads(pastebin_url)["data"][0]
+    data = {
+            "auth": auth,
+            "scheme_code": scheme_code,
+            "from": from_date,
+            "to": to_date,
+            "frequency": "month"
+            }
+    r = requests.post(url = API_ENDPOINT2, data = data)
+    pastebin_url = r.text
+    json_object1 = json.loads(pastebin_url)["data"]
+    # print(json_object[0])
+    json_object.update(json_object1)
+    # json_object = json.loads(
+    # json_object[0]
     return json_object
 
 
+# x = get_mf()
+# print(x)
 if __name__ == '__main__':
     app.run()
